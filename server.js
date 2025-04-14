2b455a0677989e64f626945f8f568945d0d82df6
ZaydAbdullayev <zeyyadabuzayd@gmail.com>
2025-04-14 14:41:18 +0500

    update

--- server.js
+++ server.js
@@ -8,20 +8,20 @@ app.use(cors());
 app.use(express.json());
 
 let votes = { support: 0, oppose: 0 };
-const voters = new Set(); // basit IP temelli oy sınırlayıcı
-const visitors = new Set(); // IP temelli ziyaretçi sınırlayıcı
+const allVoters = new Set(); // basit IP temelli oy sınırlayıcı
+const all_visitors = new Set(); // IP temelli ziyaretçi sınırlayıcı
 
 app.post("/vote", (req, res) => {
     const { vote } = req.body;
     const ip = req.ip;
 
-    if (voters.has(ip)) {
+    if (allVoters.has(ip)) {
         return res.status(403).json({ message: "You already voted." });
     }
 
     if (vote === "support" || vote === "oppose") {
         votes[vote]++;
-        voters.add(ip);
+        allVoters.add(ip);
         return res.json({ success: true });
     }
 
@@ -31,13 +31,13 @@ app.post("/vote", (req, res) => {
 app.get("/results", (req, res) => {
     const ip = req.ip;
 
-    if (!visitors.has(ip)) {
-        visitors.add(ip);
+    if (!all_visitors.has(ip)) {
+        all_visitors.add(ip);
     }
 
     res.json({
         votes: votes,
-        visitors: visitors.size,
+        visitors: all_visitors.size,
         message: "Thank you for your visit!",
     });
 });
